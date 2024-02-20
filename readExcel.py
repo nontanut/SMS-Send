9527e515fe182072423c9e1c7aa085862a8cb65f
Ptest <bo.nontanut@gmail.com>
2024-02-20 15:31:34 +0700

    connect api

--- readExcel.py
+++ readExcel.py
@@ -1,6 +1,22 @@
 import pandas as pd
+import requests
 
-df = pd.read_excel (r'PA+ NCAP 19.02.24.xlsx')
+file_input = input("File path\n").replace("& ","").replace("'","")
 
+df = pd.read_excel(file_input, keep_default_na=False)
 data = df["MobilePhone"].apply(str)
-print(','.join(data.values.tolist()))
+phone = ','.join(data.values.tolist())
+
+print(phone)
+
+data_from = {
+  "SenderId": "iCARE INSUR",
+  "GroupId": "",
+  "Is_Unicode": True,
+  "Message": "ข้อความทดสอบ",
+  "MobileNumbers": phone,
+  "ApiKey": "jR7FlmIHJrysgGZ5bSd1iJsfrsC+nX+MMFD/dvD+iP4=",
+  "ClientId": "837cd9b7-26fd-4f28-89b2-cb2a75f9f112"
+}
+res = requests.post('https://api.send-sms.in.th/api/v2/SendSMS', json=data_from)
+print(res)
